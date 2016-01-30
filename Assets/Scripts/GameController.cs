03cb398d4ee6944342a352992ff613a32d45216e
ArttuHeinonen <arttu.hei@gmail.com>
2016-01-30 21:47:54 +0200

    Liikkuminen

---
+++ Assets/Scripts/GameController.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+    public Camera cam;
+    public GameObject melon;
+    public float minSpawnTime, maxSpawnTime;
+    private float maxWidth;
+
+    void Start()
+    {
+        if(cam == null)
+        {
+            cam = Camera.main;
+        }
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0f);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        float melonWidth = melon.GetComponent<Renderer>().bounds.extents.x;
+        maxWidth = targetWidth.x - melonWidth;
+        StartCoroutine(Spawn());
+    }
+
+    IEnumerator Spawn()
+    {
+        //yield return new WaitForSeconds(1f);
+        while(true){
+            Vector3 spawnPosition = new Vector3(Random.Range(-maxWidth, maxWidth), transform.position.y, 0f);
+            Quaternion spawnRotation = Quaternion.identity;
+            Instantiate(melon, spawnPosition, spawnRotation);
+            yield return new WaitForSeconds(Random.Range(1f, 2f));
+        }
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+
+    public Camera cam;
+    private float maxWidth;
+    private Rigidbody2D rigid;
+
+	void Start () {
+	    if(cam == null)
+        {
+            cam = Camera.main;
+        }
+        rigid = GetComponent<Rigidbody2D>();
+        Vector3 upperCorner = new Vector3(Screen.width, Screen.height, 0f);
+        Vector3 targetWidth = cam.ScreenToWorldPoint(upperCorner);
+        maxWidth = targetWidth.x;
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+        Vector3 rawPosition = cam.ScreenToWorldPoint(Input.mousePosition);
+        Vector3 targetPosition = new Vector3(rawPosition.x, 0f, 0f);
+        float targetWidth = Mathf.Clamp(targetPosition.x, -maxWidth, maxWidth);
+        rigid.MovePosition(targetPosition);
+	}
+}
